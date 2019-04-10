c8dd126aa46ad28ffbb9e7908df7841db81a666c
Oscar Mason <oscar@live.co.uk>
2019-04-10 23:12:42 +0100

    Add separate classes for task1 and task2

---
+++ src/main/java/com/booking/go/assessment/Task1.java
@@ -0,0 +1,67 @@
+package com.booking.go.assessment;
+
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class Task1 {
+    private static String format = "Please enter the pickup and dropoff points as follows" +
+            "along with an optional value for the number of passengers:\n" +
+            " com.booking.go.assessment.Task1 pickup_lat pickup_long dropoff_lat dropoff_long num_passengers\n" +
+            "For example:\n" +
+            " com.booking.go.assessment.Task1 3.410632 -2.157533 3.410632 -2.157533 4";
+
+    public static void main(String[] args) {
+        int numArgs = args.length;
+
+        int i = 0;
+        boolean getAllResults = true;
+
+        // Check for option
+        if((numArgs == 5 || numArgs == 6) && args[i].charAt(0) == '-'){
+            if(args[i].length() > 1 && args[i].charAt(1) == 'd'){
+                getAllResults = false;
+            }else{
+                System.out.println("Unsupported argument");
+                return;
+            }
+            i++;
+        }
+
+        Trip trip = null;
+        try{
+            if(numArgs - i == 4){
+                trip = new InputParser().parseTripInput(args[i], args[i+1], args[i+2], args[i+3]);
+            }else if(numArgs - i == 5){
+                trip = new InputParser().parseTripInput(args[i], args[i+1], args[i+2], args[i+3], args[i+4]);
+            }else{
+                System.out.println(format);
+                return;
+            }
+        }catch(InvalidInputException e){
+            System.out.println(format);
+            return;
+        }
+
+        PriorityQueue<Ride> allRides = null;
+
+        if(getAllResults){
+            allRides = new PickupRequester().queryRidesFromAllSuppliers(trip);
+        }else{
+            Set<Ride> rides = new PickupRequester().getSupplierResults(trip, Supplier.DAVE);
+            allRides = new PickupRequester().orderRidesByPriceAscending(rides);
+        }
+
+        if(allRides.isEmpty()){
+            System.out.println("Unfortunately there are not cars available at this time");
+        }
+
+        while(!allRides.isEmpty()){
+            Ride ride = allRides.poll();
+            if(getAllResults){
+                System.out.println(ride.getCar().CAR_TYPE + " - " + ride.getSupplier() + " - " + ride.getPrice());
+            }else{
+                System.out.println(ride.getCar().CAR_TYPE + " - " + ride.getPrice());
+            }
+        }
+    }
+}
---
+++ src/main/java/com/booking/go/assessment/Task2.java
@@ -0,0 +1,12 @@
+package com.booking.go.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task2.class, args);
+    }
+}
