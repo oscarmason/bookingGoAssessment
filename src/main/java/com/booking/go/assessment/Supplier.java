1e1c7c4f15cd6b99db0345d5cdf7bcf1dc737385
Oscar Mason <oscar@live.co.uk>
2019-04-10 16:14:12 +0100

    Modify toString method of Supplier to support sentence case

--- src/main/java/com/booking/go/assessment/Supplier.java
+++ src/main/java/com/booking/go/assessment/Supplier.java
@@ -5,6 +5,6 @@ public enum Supplier {
 
     @Override
     public String toString(){
-        return this.name().toLowerCase();
+        return this.name().substring(0, 1).toUpperCase() + this.name().substring(1).toLowerCase();
     }
 }
