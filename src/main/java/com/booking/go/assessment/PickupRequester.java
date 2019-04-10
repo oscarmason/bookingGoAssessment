f30e8c474f62ac73827f36e12c9f2fd14dd92800
Oscar Mason <oscar@live.co.uk>
2019-04-10 16:13:13 +0100

    Add ability to request all suitable cars for each supplier's API and printing each type in order

--- src/main/java/com/booking/go/assessment/PickupRequester.java
+++ src/main/java/com/booking/go/assessment/PickupRequester.java
@@ -28,6 +28,42 @@ class PickupRequester {
         return extractCarsFromResponse(response, numPassengers, supplier);
     }
 
+    void pickupRequest(Journey journey, int numPassengers){
+        Map<String, Car> cars = new HashMap<>();
+
+        for(Supplier supplier : Supplier.values()){
+            Set<Car> carsFromSupplier = getSupplierResults(journey, numPassengers, supplier);
+
+            for(Car car : carsFromSupplier){
+                if(!cars.containsKey(car.CAR_TYPE) || car.getPrice() < cars.get(car.CAR_TYPE).getPrice()){
+                    cars.put(car.CAR_TYPE, car);
+                }
+            }
+        }
+
+        PriorityQueue<Car> orderedCars = orderCars(cars);
+        printSearchResults(orderedCars);
+    }
+
+    private PriorityQueue<Car> orderCars(Map<String, Car> cars){
+        PriorityQueue<Car> carsOrderedByPrice =  new PriorityQueue<>(
+                Comparator.comparing(Car::getPrice));
+
+        carsOrderedByPrice.addAll(cars.values());
+        return carsOrderedByPrice;
+    }
+
+    private void printSearchResults(PriorityQueue<Car> cars){
+        if(cars.isEmpty()){
+            System.out.println("Unfortunately there are not cars available at this time");
+        }
+
+        while(!cars.isEmpty()){
+            Car car = cars.poll();
+            System.out.println(car.CAR_TYPE + " - " + car.getRegisteredSupplier() + " - " + car.getPrice());
+        }
+    }
+
     private Map<String, String> journeyRequestLocationsToParameters(Journey journey){
         Map<String, String> parameters = new HashMap<>();
         parameters.put("pickup", journey.getPickupLat() + "," + journey.getPickupLong());
