ad325a27c2da9bb84d215e4faaccbcc1e0476f9e
Oscar Mason <oscar@live.co.uk>
2019-04-10 23:10:30 +0100

    Split location arguments of input parser and add overloaded function to support number of passengers

--- src/main/java/com/booking/go/assessment/InputParser.java
+++ src/main/java/com/booking/go/assessment/InputParser.java
@@ -1,4 +1,30 @@
 package com.booking.go.assessment;
 
+import java.util.Arrays;
+
 public class InputParser {
+
+    public Trip parseTripInput(String pickupLat, String pickupLong, String dropoffLat,
+                   String dropoffLong){
+        return parseTripInput(pickupLat, pickupLong, dropoffLat, dropoffLong, "1");
+    }
+
+    public Trip parseTripInput(String pickupLat, String pickupLong, String dropoffLat,
+                               String dropoffLong, String numPassengers){
+
+        Trip trip = null;
+        try{
+            int passengers = Integer.parseInt(numPassengers);
+            if(passengers <= 0){
+                throw new InvalidInputException(InvalidInputException.INVALID_NUMBER_OF_PASSENGERS);
+            }
+
+            trip = new Trip(Double.parseDouble(pickupLat), Double.parseDouble(pickupLong),
+                    Double.parseDouble(dropoffLat), Double.parseDouble(dropoffLong), passengers);
+        }catch (NumberFormatException e){
+            throw new InvalidInputException("Input not valid.");
+        }
+
+        return trip;
+    }
 }
