26459a52ea9968eb1e8623b480f755294227f15c
Oscar Mason <oscar@live.co.uk>
2019-04-11 01:44:36 +0100

    Return null Trip object when provided incorrect input

--- src/main/java/com/booking/go/assessment/InputParser.java
+++ src/main/java/com/booking/go/assessment/InputParser.java
@@ -18,18 +18,17 @@ public class InputParser {
      */
     public Trip parseTripInput(String pickupLat, String pickupLong, String dropoffLat,
                                String dropoffLong, String numPassengers){
-
         Trip trip = null;
         try{
             int passengers = Integer.parseInt(numPassengers);
             if(passengers <= 0){
-                throw new InvalidInputException(InvalidInputException.INVALID_NUMBER_OF_PASSENGERS);
+                return null;
             }
 
             trip = new Trip(Double.parseDouble(pickupLat), Double.parseDouble(pickupLong),
                     Double.parseDouble(dropoffLat), Double.parseDouble(dropoffLong), passengers);
         }catch (NumberFormatException e){
-            throw new InvalidInputException("Input not valid.");
+            return null;
         }
 
         return trip;
